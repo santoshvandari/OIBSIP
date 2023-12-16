89c9222357270450713e499a90fb9af07964372e
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-16 16:10:06 +0545

    working Todo's

---
+++ ToDoWebApp/script.js
@@ -0,0 +1,10 @@
+// // task list template
+// <tr>
+//     <th scope="row">1</th>
+//     <td>Buy groceries for next week</td>
+//     <td>In progress</td>
+//     <td>
+//         <button type="submit" class="btn btn-danger">Delete</button>
+//         <button type="submit" class="btn btn-success ms-1">Finished</button>
+//     </td>
+// </tr>
