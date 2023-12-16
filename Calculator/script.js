5ca9550ee4a6b308dc93b4d8e83f0bc7a0452530
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-16 14:48:04 +0545

    Final Calc is Ready

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,28 +1,49 @@
 let inputEl=document.getElementById('input');
 let btnEl=Array.from(document.getElementsByTagName('button'));
-
-console.log(btnEl);
+let prevans=0;
+inputEl.value=0;
 btnEl.forEach(element => {
     element.addEventListener('click',function(){
-        if(element.textContent=='C'){
+        if(inputEl.value==0){
             inputEl.value='';
-            return;
         }
-        if(element.textContent=='='){
+        if(element.textContent=='clr'){
+            inputEl.value='0';
+        }else if(element.textContent=='='){
             data=inputEl.value;
+            if(data.includes('Error') || data.includes('undefined')){
+                data=0;
+            }
             Calculate(data);
-        }
-        if(element.textContent!='=')
+        }else if(element.textContent=='del'){
+            if((inputEl.value).includes('Error') || (inputEl.value).includes('undefined')){
+                inputEl.value=0;
+            }
+            inputEl.value=inputEl.value.slice(0,-1);
+            if(inputEl.value==''){
+                inputEl.value=0;
+            }
+        }else if(element.textContent=='ans'){
+            inputEl.value+=prevans;
+        }else{
             inputEl.value+=element.textContent;
+            santosh=1;
+        }
+        
     });
-    console.log(element)
 });
-
 function Calculate(data){
     try{
         res=eval(data);
     }catch(err){
         res='Error';
     }
+    if(res==undefined){
+        res='0';
+    }
     inputEl.value=res;
+    prevans=res;
+    if(res=='Error' || res=='undefined'){
+        prevans=0;
+    }
 }
