55b31201798361b02a3dc624fff0f908fdc75930
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-16 13:41:20 +0545

    Initial Calculator Is Ready

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,10 +1,28 @@
 let inputEl=document.getElementById('input');
-let btnEl=document.getElementsByTagName('button');
+let btnEl=Array.from(document.getElementsByTagName('button'));
 
+console.log(btnEl);
 btnEl.forEach(element => {
+    element.addEventListener('click',function(){
+        if(element.textContent=='C'){
+            inputEl.value='';
+            return;
+        }
+        if(element.textContent=='='){
+            data=inputEl.value;
+            Calculate(data);
+        }
+        if(element.textContent!='=')
+            inputEl.value+=element.textContent;
+    });
     console.log(element)
 });
 
-function Calculate(){
-	eval(res);
+function Calculate(data){
+    try{
+        res=eval(data);
+    }catch(err){
+        res='Error';
+    }
+    inputEl.value=res;
 }
