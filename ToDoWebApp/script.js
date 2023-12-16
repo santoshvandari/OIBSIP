f8c4bbd240b34d44b82f62494c7f74ba1921d3fa
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-16 19:44:31 +0545

    Initial

--- ToDoWebApp/script.js
+++ ToDoWebApp/script.js
@@ -8,3 +8,64 @@
 //         <button type="submit" class="btn btn-success ms-1">Finished</button>
 //     </td>
 // </tr>
+
+//Creating the Local Storage to SetData
+let taskList = [];
+function setData(task) {
+    if(getItem(task)==false){
+        // console.log(getItem(task))
+        if(task){
+            // console.log(task)
+            taskList=getData() || [];
+            taskList.push(task);
+            data=JSON.stringify(taskList)
+            console.log(data)
+            localStorage.setItem("tasklist",JSON.stringify(taskList));
+        }else{
+            console.log("Task is empty");
+        }
+    }else{
+        console.log("Task already exists");
+    }
+}
+function getData() {
+    let str = localStorage.getItem("tasklist");
+    if (str != null) {
+        // console.log(str)
+        taskList = JSON.parse(str);
+        // console.log(taskList)
+        return taskList;
+    }else{
+        return null;
+    }
+}
+
+//Reading Item
+function getItem(item){
+    let str = localStorage.getItem('tasklist')
+    let task  = JSON.parse(str)
+    // console.log(task)
+    if(task!=null){
+        let dataindex= task.indexOf(item)
+        if(dataindex==-1){
+            return false
+        }else{
+            return true
+        }
+    }else{
+        return false
+    }
+}
+
+
+// data=['santosh','helloq']
+// indexdata=data.indexOf("hello")
+// console.log(indexdata)
+
+setData('test')
+setData('test')
+setData('santosh')
+setData('bhandari')
+setData('hello')
+setData('tasfasf')
+// getData();
