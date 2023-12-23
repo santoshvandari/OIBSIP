ea3a604971a308776f1eccadb6c4a0d542d8b930
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-23 09:57:32 +0545

    Initial

---
+++ LoginAuthentication/static/login/script.js
@@ -0,0 +1,8 @@
+// 'use strict';
+// document.getElementById('form').addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     var email = document.getElementById('email').value;
+//     var password = document.getElementById('password').value;
+//     if (email === '' || password === '') {
+//         alert('Please fill out all fields');
+//     }
