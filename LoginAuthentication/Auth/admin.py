3299e08ad8af1c1dee95aa0e34fc2476ea07c334
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-24 19:50:59 +0545

    Updated

--- LoginAuthentication/Auth/admin.py
+++ LoginAuthentication/Auth/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import UserDetails
 
 # Register your models here.
-class UserDetailsDisplay(admin.ModelAdmin):
-    list_display=['fullname','email','get_username']
+# class UserDetailsDisplay(admin.ModelAdmin):
+#     list_display=['fullname','email','get_username']
 
-    def get_username(self, obj):
-        return obj.user.username
+#     def get_username(self, obj):
+#         return obj.user.username
 
-    get_username.short_description = 'Username'
+#     get_username.short_description = 'Username'
 
-admin.site.register(UserDetails,UserDetailsDisplay)
+# admin.site.register(UserDetails,UserDetailsDisplay)
