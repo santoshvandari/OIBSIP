77a4b3d3f8546ef0aeb095bf2235187251bbf837
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-24 19:50:13 +0545

    Updated

--- LoginAuthentication/Auth/models.py
+++ LoginAuthentication/Auth/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class UserDetails(models.Model):
-    # user will store username and password only
-    user=models.OneToOneField(User,on_delete=models.CASCADE)
-    # add more fields
-    fullname=models.CharField(max_length=100)
-    email=models.EmailField()
+# class UserDetails(models.Model):
+#     # user will store username and password only
+#     user=models.OneToOneField(User,on_delete=models.CASCADE)
+#     # add more fields
+#     fullname=models.CharField(max_length=100)
+#     email=models.EmailField()
 
-    class Meta:
-        db_table='userdetails'
-        ordering=['user']
+#     class Meta:
+#         db_table='userdetails'
+#         ordering=['user']
