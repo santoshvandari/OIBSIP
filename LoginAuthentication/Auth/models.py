75bef53d890c4ac1e5309a327f2c2d30c98c1d69
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-24 19:35:20 +0545

    Updated

--- LoginAuthentication/Auth/models.py
+++ LoginAuthentication/Auth/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserDetails(models.Model):
+    # user will store username and password only
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    # add more fields
+    fullname=models.CharField(max_length=100)
+    email=models.EmailField()
--- LoginAuthentication/Auth/views.py
+++ LoginAuthentication/Auth/views.py
@@ -1,14 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+# import login method 
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def log_in(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        # authenticate user
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('/')
+        else:
+            return render(request,'login.html',{'error':'Invalid Credentials'})
     return render(request,'login.html')
 
 def log_out(request):
-    return render(request,'logout.html')
-
+    logout(request)
+    return redirect('/login')
 def home(request):
+    if not request.user.is_authenticated:
+        return redirect('/login')
     return render(request,'index.html')
 
 def sign_up(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        pass
     return render(request,'signup.html')
