bedb9a5cf9ea98b2bda85c48ed2834896cc3ecb0
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-12-25 18:28:41 +0545

    Final Code of Portfolio Website Completed

--- PortfolioWebsite/script.js
+++ PortfolioWebsite/script.js
@@ -1,6 +1,23 @@
 'use strict';
 (function(){
+    //AOS INIT
+    AOS.init({
+        delay: 200,
+        duration: 1000,
+
+    });
+    // preloader Code 
     window.addEventListener('load', function(){
         document.querySelector('div.preloader').style='display:none';
     })
+    // typig Effect Code 
+    let typed = new Typed('#typing', {
+        strings: ['Front End Developer', 'Back End Developer','Wordpress Development','Grapic Designer','Freelancer'],
+        typeSpeed: 100,
+        loop: true,
+        backSpeed: 100,
+        backDelay: 1000,
+        shuffle: true,
+      });
+
 })();
